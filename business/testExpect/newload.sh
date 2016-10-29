b9487af6dbf6ee56673548218d3e79de023af6f7
junn <junnflow@gmail.com>
2016-10-29 23:01:18 +0800

    testVxworks

--- business/testExpect/newload.sh
+++ business/testExpect/newload.sh
@@ -5,23 +5,28 @@ declare -a array_ip
 touch .temp.txt
 
 num=`cat ip_config.cfg | wc -l`
-function vxworks(){
-    echo $1
+
+function get_date() {
+    echo -n `date +"%Y.%m.%d-%H:%M"`
 }
 
-function init_prepare(){
+dir_name=`get_date`
+mkdir $dir_name
 
-    for ((i=1;i<$num;i++));
-    do
-        cat ip_config.cfg | awk '{if(NR=='''$i'''){print $2}}' | xargs -i `vxworks`
-        sleep 10
+function important_test(){
+    cd $dir_name
+    for (( i = 1; i <=$num ; i++ )); do
+        #statements
+        array_slots[$i]=`cat ip_config.cfg | awk '{if(NR=='''$i'''){ print $1 }}'`
+        touch ${array_slots[$i]}
+        array_ip[$i]=`cat ip_config.cfg | awk '{if(NR=='''$i'''){ print $2 }}'`
     done
 }
+
 
 # `init_prepare`
 
 function get_mem() {
-    
     /usr/bin/expect<<EXP
     spawn telnet $1
     expect "login"
@@ -35,11 +40,9 @@ function get_mem() {
 EXP
     free=`cat .temp.txt | grep -e 'free' | awk '{print $2}'`
     alloc=`cat .temp.txt | grep -e 'allo' | awk '{if(NR==1){print $2}}'`
-    result=$[$alloc/$[$free+$alloc]
-
+    result=`echo $free`
     ############!!!slots type!!
     echo -ne "" > .temp.txt
-
 }
 
 
@@ -75,3 +78,6 @@ EXP
     cat .temp.txt | grep 'Bmu' | awk '{print $4/3600"Hours"}' 
     echo -ne "" > .temp.txt
 }
+
+#cat value from .cfg
+important_test 
